04342d441460f681fd73ccbfa0160ff22d3f78d4
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-11-05 01:39:41 +0200

    gix inputs

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,8 +1,8 @@
 document.body.onload = function () {
 
-setTimeout(function() {
-    var preloader = document.getElementById('preloader');
-    if (!preloader.classList.contains('disable')) {
-        preloader.classList.add('disable')
-    }},1000)
-}
+    setTimeout(function() {
+        var preloader = document.getElementById('preloader');
+        if (!preloader.classList.contains('disable')) {
+            preloader.classList.add('disable')
+        }},1000)
+    }
